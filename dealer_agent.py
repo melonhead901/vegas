6695fb45127bc2872f22902d051185628537ad24
Kellen Donohue <kellend@cs.washington.edu>
2012-11-25 18:06:02 -0800

    Added dealer agent

---
+++ dealer_agent.py
@@ -0,0 +1,20 @@
+
+from agent import Agent
+from actions import Actions
+
+"""
+Hand for the dealer, which has no choices.
+
+Most casinos play dealer must stand on soft 17 and anything below.
+Sometimes the delaer must hit on soft 17. To make this change in the
+code change the check to be ... softCount() <= 17 instead of < 17.
+"""
+class DealerAgent(Agent):  
+    def receiveHand(self, hand):
+      self.hand = hand
+
+    def getNextAction(self, hand, handList):
+      if self.hand.getSoftCount() < 17:
+        return Actions.HIT
+      else:
+        return Actions.STAND      
